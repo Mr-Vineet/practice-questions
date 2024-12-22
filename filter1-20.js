0fa8a260497519a224d3f47d292f5d7b8513b8e7
Vineet <100793773+Mr-Vineet@users.noreply.github.com>
2024-12-22 18:09:24 +0530

    modified filterLongWords function

--- filter1-20.js
+++ filter1-20.js
@@ -6,10 +6,18 @@ const filterEvenNumbers = function (numbers) {
   return numbers.filter(isEven);
 };
 
-console.log(filterEvenNumbers([1, 2, 3, 4, 5]));
+// console.log(filterEvenNumbers([1, 2, 3, 4, 5]));
 
 // words with more than 5 letters ["apple", "banana", "kiwi", "grape"] => ["banana"]
-const filterLongWords = function (words) { };
+const hasMoreThan5Letters = function (word) {
+  return word.length > 5;
+};
+
+const filterLongWords = function (words) {
+  return words.filter(hasMoreThan5Letters);
+};
+
+console.log(filterLongWords(["apple", "banana", "kiwi", "grape"]));
 
 // people older than 30 [{name: "Alice", age: 25}, {name: "Bob", age: 35}] => [{name: "Bob", age: 35}]
 const filterAdults = function (people) { };
