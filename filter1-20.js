572c42609529dc3644782d7a7782df83238b4053
Vineet <100793773+Mr-Vineet@users.noreply.github.com>
2024-12-22 21:58:24 +0530

    modified filterRecentOrders function

--- filter1-20.js
+++ filter1-20.js
@@ -69,13 +69,21 @@ const filterHighGrades = function (students) {
   });
 };
 
-console.log(filterHighGrades([{ name: "John", grade: 75 }, { name: "Jane", grade: 85 }]));
+// console.log(filterHighGrades([{ name: "John", grade: 75 }, { name: "Jane", grade: 85 }]));
 
 // products that are in stock [{product: "apple", inStock: true}, {product: "banana", inStock: false}] => [{product: "apple", inStock: true}]
-const filterInStockProducts = function (products) { };
+const filterInStockProducts = function (products) {
+  return products.filter(function (product) {
+    return product.inStock;
+  });
+};
+
+console.log(filterInStockProducts([{ product: "apple", inStock: true }, { product: "banana", inStock: false }]));
 
 // orders placed in the last 30 days [{orderDate: "2024-11-01"}, {orderDate: "2024-12-01"}] => [{orderDate: "2024-12-01"}]
-const filterRecentOrders = function (orders) { };
+const filterRecentOrders = function (orders) {
+  
+ };
 
 //============================= 10 Qs =======================================//
 
